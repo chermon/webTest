90e6f59f3c0a2807d35abeba7b48ea022bc103d6
chermon <chermon15@163.com>
2020-05-21 19:00:53 +0800

    code

--- vue-router-test/src/views/Detail.vue
+++ vue-router-test/src/views/Detail.vue
@@ -1,15 +1,46 @@
 <template>
-    <div></div>
+    <div v-if='filminfo' class="filminfo">
+      <img :src="filminfo.poster" alt="" class="infopic">
+      <div class='introction'>
+        <h2>{{filminfo.name}}</h2>
+        <p>{{filminfo.category}}</p>
+        <p>{{filminfo.nation}} <span>|</span> {{filminfo.runtime}}</p>
+      </div>
+      <div class="actors">
+        <h3>演职人员</h3>
+        <swiper class="actorswiper" myswiper="actorswiper" picnumber='4'>
+          <div class="swiper-slide" v-for="actor in filminfo.actors" :key="actor.name">
+            <img :src="actor.avatarAddress" alt="">
+            <p class="actorname">{{actor.name}}</p>
+            <p class="actorrole">{{actor.role}}</p>
+          </div>
+        </swiper>
+      </div>
+      <div class="teampic">
+        <h3>剧照</h3>
+        <swiper class="teamswiper" myswiper="teamswiper" picnumber='3'>
+          <div class="swiper-slide" v-for="(photo,index) in filminfo.photos" :key="index">
+            <img :src="photo" alt="">
+          </div>
+        </swiper>
+      </div>
+    </div>
 </template>
 <script>
 import axios from 'axios'
+import swiper from './Detail/DetailSwiper'
+import Vue from 'vue'
+
 export default {
   data: function () {
-    return { filmId: '' }
+    return { filmId: '',filminfo: null}
+  },
+  components: {
+    swiper
   },
   mounted: function () {
     this.filmId = this.$route.params.id
-    console.log(this.$route.params.id)
+    console.log(this.filmId)
     axios({
       url: `https://m.maizuo.com/gateway?filmId=${this.filmId}&k=2788033`,
       headers: {
@@ -17,9 +48,55 @@ export default {
         'X-Host': 'mall.film-ticket.film.info'
       }
     }).then(response => {
-      console.log(response.data)
+      console.log(response.data.data.film)
+      this.filminfo = response.data.data.film;
     })
   }
-
 }
 </script>
+<style lang="scss" scoped>
+.filminfo{
+  background-color: rgb(250, 246, 246);
+  color: rgb(43, 42, 42);
+  h2{
+    line-height: 38px;
+  }
+  .infopic{
+      width: 100%; 
+  }
+  .introction{
+    background-color: white;
+    padding:10px 10px;
+    p{
+      color: rgb(156, 155, 155);
+      font-size: 14px;
+      line-height: 24px;
+      span{
+        padding: 0 5px 0;
+      }
+     }
+  }
+  
+  .actors{
+    margin-top: 10px;
+    background-color: white;
+    padding: 10px 0px 5px 10px;
+    h3{
+      line-height: 30px;
+      font-weight: normal;
+    }
+    .actorname{
+      font-size: 14px;
+    }
+    .actorrole{
+      font-size: 14px;
+      color: rgb(156, 155, 155);
+    }
+    .actorswiper{
+      margin-top: 15px;
+    }
+  }
+  
+}
+
+</style>
---
+++ vue-router-test/src/views/Detail/DetailSwiper.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="swiper-container myswiper">
+        <div class="swiper-wrapper">
+            <slot></slot>
+        </div>
+        <!-- 分页器 -->
+        <div class="swiper-pagination"></div>
+    </div>
+</template>
+<script>
+import Swiper from 'swiper'
+import 'swiper/css/swiper.css'
+export default {
+    props:["myswiper","picnumber"],
+    mounted: function (){
+        new Swiper('.'+this.myswiper, {
+            slidesPerView: this.picnumber,
+            spaceBetween: this.picnumber === '3'? 30:20,
+        });
+    }
+}
+</script>
+<style lang="scss" scoped>
+.detailswiper{
+    img{
+        width: 100%;
+    }
+}
+</style>
